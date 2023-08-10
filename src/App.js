e7aa96d2c84dba743ca7112f5dc643fa324ae28b
berluche <berlucheprunier@gmail.com>
2023-08-10 18:02:10 -0400

    committing

--- src/App.js
+++ src/App.js
@@ -5,15 +5,27 @@ import Nav from "./components/Nav";
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import { books } from "./data"
 import BooksInfo from "./Pages/BooksInfo";
+import Cart from "./Pages/Cart";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [cart, setCart ]= useState ([])
+
+  function addToCart(book) {
+    setCart([...cart, book])
+  }
+
+  useEffect (() => {
+    
+  }, [cart])
   return (
     < Router>
     <div className="App">
       <Nav />
       <Route path="/" exact component={Home} />
-      <Route path="/books" component={()=> <Book books={books}/>} />
-      <Route path="/books/1" component={()=> <BooksInfo books={books}/>} />
-     
+      <Route path="/books" exact render={()=> <Book books={books}/>} />
+      <Route path="/books/:id" render={()=> <BooksInfo books={books} addToCart={addToCart}/>} />
+      <Route path="/cart" render={()=> <Cart books={books}/>} />
       <Footer/>
     </div>
     </Router>
